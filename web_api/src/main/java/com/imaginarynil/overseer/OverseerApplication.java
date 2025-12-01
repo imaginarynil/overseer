12de81f967f693266ed243d9efd49b553a67b316
Daniel Sugianto <danielsugianto.it@gmail.com>
2025-12-01 10:44:01 -0500

    init spring boot

---
+++ web_api/src/main/java/com/imaginarynil/overseer/OverseerApplication.java
@@ -0,0 +1,13 @@
+package com.imaginarynil.overseer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverseerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OverseerApplication.class, args);
+	}
+
+}
