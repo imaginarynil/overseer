71fa472bce4b71cab837392f2485736d2332950c
Daniel Sugianto <danielsugianto.it@gmail.com>
2025-12-09 15:45:16 -0500

    add db schema

---
+++ web_api/src/main/resources/schema.sql
@@ -0,0 +1,69 @@
+DROP DATABASE IF EXISTS practice;
+CREATE DATABASE practice;
+USE practice;
+
+SELECT DATABASE();
+
+CREATE TABLE IF NOT EXISTS admin (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(50) NOT NULL,
+    password VARCHAR(20) NOT NULL,
+    creation_date DATETIME NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS manager (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(100) NOT NULL,
+    username VARCHAR(20) NOT NULL,
+    password VARCHAR(20) NOT NULL,
+    phone_number VARCHAR(20) NOT NULL,
+    creation_date DATETIME NOT NULL  DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS employee (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    manager_id INT NOT NULL,
+	name VARCHAR(100) NOT NULL,
+    username VARCHAR(20) NOT NULL,
+    password VARCHAR(20) NOT NULL,
+    phone_number VARCHAR(20) NOT NULL,
+	latitude DECIMAL(8, 6),
+	longitude DECIMAL(9, 6),
+    creation_date DATETIME NOT NULL DEFAULT NOW()
+);
+ALTER TABLE employee ADD FOREIGN KEY (manager_id) REFERENCES manager(id);
+
+CREATE TABLE IF NOT EXISTS client (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(100) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    phone_number VARCHAR(20) NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    creation_date DATETIME NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS job (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    client_id INT NOT NULL,
+	name VARCHAR(100) NOT NULL,
+    description TEXT NOT NULL,
+    start_time DATETIME NOT NULL,
+    end_time DATETIME NOT NULL,
+    address VARCHAR(100) NOT NULL,
+	latitude DECIMAL(8, 6) NOT NULL,
+	longitude DECIMAL(9, 6) NOT NULL,
+    creation_date DATETIME NOT NULL DEFAULT NOW(),
+    update_date DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW()
+);
+ALTER TABLE job ADD FOREIGN KEY (client_id) REFERENCES client(id);
+
+CREATE TABLE IF NOT EXISTS employee_job (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	employee_id INT NOT NULL,
+    job_id INT NOT NULL,
+    start_time DATETIME NOT NULL,
+    end_time DATETIME NOT NULL,
+    creation_date DATETIME NOT NULL DEFAULT NOW()
+);
+ALTER TABLE employee_job ADD FOREIGN KEY (employee_id) REFERENCES employee(id);
+ALTER TABLE employee_job ADD FOREIGN KEY (job_id) REFERENCES job(id);
