177060040d30742162aafd72582dcf0fb39189bf
Daniel Sugianto <danielsugianto.it@gmail.com>
2026-04-15 17:01:05 +0800

    add set manager id to null when a manager is deleted

--- web_api/db/migration/V1_0_0__create_basic_tables.sql
+++ web_api/db/migration/V1_0_0__create_basic_tables.sql
@@ -16,7 +16,8 @@ CREATE TABLE IF NOT EXISTS manager (
 
 CREATE TABLE IF NOT EXISTS employee (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    manager_id INT NOT NULL,
+    -- when assigning new employees to a manager, an employee can be without a manager
+    manager_id INT,
 	name VARCHAR(100) NOT NULL,
     username VARCHAR(20) NOT NULL,
     password VARCHAR(20) NOT NULL,
@@ -25,7 +26,27 @@ CREATE TABLE IF NOT EXISTS employee (
 	longitude DECIMAL(9, 6),
     creation_date DATETIME NOT NULL DEFAULT NOW()
 );
-ALTER TABLE employee ADD FOREIGN KEY (manager_id) REFERENCES manager(id);
+ALTER TABLE employee
+    ADD CONSTRAINT fk_employee_manager
+    FOREIGN KEY (manager_id) REFERENCES manager(id) ON DELETE SET NULL;
+
+/*
+-- modify manager_id to nullable
+ALTER TABLE employee MODIFY COLUMN manager_id INT NULL;
+
+-- add on delete set null
+
+-- find fk constraint name
+SHOW CREATE TABLE employee;
+-- drop fk constraint
+ALTER TABLE employee DROP FOREIGN KEY employee_ibfk_1;
+-- add on delete cascade
+ALTER TABLE employee
+    ADD CONSTRAINT fk_employee_manager
+    FOREIGN KEY (manager_id)
+    REFERENCES manager(id)
+    ON DELETE SET NULL;
+*/
 
 CREATE TABLE IF NOT EXISTS client (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -51,7 +72,9 @@ CREATE TABLE IF NOT EXISTS job (
     creation_date DATETIME NOT NULL DEFAULT NOW(),
     update_date DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW()
 );
-ALTER TABLE job ADD FOREIGN KEY (client_id) REFERENCES client(id);
+ALTER TABLE job
+    ADD CONSTRAINT fk_job_client
+    FOREIGN KEY (client_id) REFERENCES client(id);
 
 CREATE TABLE IF NOT EXISTS employee_job (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -61,5 +84,9 @@ CREATE TABLE IF NOT EXISTS employee_job (
     end_time DATETIME NOT NULL,
     creation_date DATETIME NOT NULL DEFAULT NOW()
 );
-ALTER TABLE employee_job ADD FOREIGN KEY (employee_id) REFERENCES employee(id);
-ALTER TABLE employee_job ADD FOREIGN KEY (job_id) REFERENCES job(id);
+ALTER TABLE employee_job
+    ADD CONSTRAINT fk_employee_job_employee
+    FOREIGN KEY (employee_id) REFERENCES employee(id);
+ALTER TABLE employee_job
+    ADD CONSTRAINT fk_employee_job_job
+    FOREIGN KEY (job_id) REFERENCES job(id);
